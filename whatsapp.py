8ae40c81237857636a5a7026cd78b142cd3d2e9d
joel2009gh <joel2009github@gmail.com>
2025-04-18 10:37:03 +0200

    tool written in pythonb

---
+++ whatsapp.py
@@ -0,0 +1,5 @@
+import pywhatkit as pwk
+phone_number = "Phone number here"
+message = "Hello World"
+pwk.sendwhatmsg_instantly(phone_number, message, wait_time=15)
+#pwk.sendwhatmsg_inlandddy(pN_ms)
